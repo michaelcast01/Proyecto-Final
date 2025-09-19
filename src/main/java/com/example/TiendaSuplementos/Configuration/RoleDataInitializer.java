48b259fd3addf889e79fe8b91898139cf859c126
michaelcast01 <michael2003cc@gmail.com>
2025-09-18 22:33:59 -0500

    Role

--- src/main/java/com/example/TiendaSuplementos/Configuration/RoleDataInitializer.java
+++ src/main/java/com/example/TiendaSuplementos/Configuration/RoleDataInitializer.java
@@ -17,16 +17,15 @@ public class RoleDataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // If no roles exist, create sensible defaults
         List<Roles> roles = rolesRepository.findAll();
         if (roles.isEmpty()) {
-            Roles admin = new Roles();
-            admin.setName("ADMIN");
-            rolesRepository.save(admin);
-
             Roles user = new Roles();
             user.setName("USER");
             rolesRepository.save(user);
+
+            Roles admin = new Roles();
+            admin.setName("ADMIN");
+            rolesRepository.save(admin);
         }
     }
 }
